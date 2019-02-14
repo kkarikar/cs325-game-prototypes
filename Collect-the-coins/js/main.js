c588d5b9e0830787a7253b5534c331117e6e44f4
kkarikar <47336266+kkarikar@users.noreply.github.com>
2019-02-14 13:59:52 -0500

    Collect-the-coins

--- Collect-the-coins/js/main.js
+++ Collect-the-coins/js/main.js
@@ -2,7 +2,7 @@
 
 window.onload = function() {
 
-  var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render});
+  var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'game', { preload: preload, create: create, update: update, render: render});
 
   function preload() {
 
